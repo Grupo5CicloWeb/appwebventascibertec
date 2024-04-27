a673e3ee1155fad88a14dc794f34a006ef76b57b
Grupo5CicloWeb <cicloc504@gmail.com>
2024-04-27 14:31:24 -0500

    mantenimiento producto terminado

--- src/main/resources/static/js/web/backoffice/product/frmproduct.js
+++ src/main/resources/static/js/web/backoffice/product/frmproduct.js
@@ -4,6 +4,9 @@ $(document).on("click", "#btnagregar", function(){
     $("#cbocategory").empty();
     $("#cbosupplier").empty();
     listarCategoriasProveedores(0, 0);
+    $("#switchproduct").hide();
+    $("#cbdiscontinued").prop("checked", false);
+    $("#hddcodprod").val("0");
     $("#modalproduct").modal("show");
 });
 
@@ -14,7 +17,35 @@ $(document).on("click", ".btnactualizar", function(){
     $("#cbosupplier").empty();
     listarCategoriasProveedores($(this).attr("data-prodcategory"),
                         $(this).attr("data-prodsupplier"));
+    $("#switchproduct").show();
+    if($(this).attr("data-proddiscontinued") === "true"){
+        $("#cbdiscontinued").prop("checked", true);
+    }else
+        $("#cbdiscontinued").prop("checked", false);
+    $("#hddcodprod").val($(this).attr("data-prodcod"));
     $("#modalproduct").modal("show");
+});
+$(document).on("click", "#btnguardar", function(){
+    $.ajax({
+        type: "POST",
+        url: "/product/register",
+        contentType: "application/json",
+        data: JSON.stringify({
+            productid: $("#hddcodprod").val(),
+            productname: $("#txtnomproduct").val(),
+            unitprice: $("#txtunitprice").val(),
+            categoryid: $("#cbocategory").val(),
+            supplierid: $("#cbosupplier").val(),
+            discontinued: $("#cbdiscontinued").prop("checked")
+        }),
+        success: function(resultado){
+            if(resultado.resultado)
+                listarProductos();
+            alert(resultado.mensaje);
+
+        }
+    });
+    $("#modalproduct").modal("hide");
 });
 
 function listarCategoriasProveedores(idcategoria, idproveedor){
@@ -46,5 +77,31 @@ function listarCategoriasProveedores(idcategoria, idproveedor){
             });
         }
     });
+}
 
+function listarProductos(){
+    $.ajax({
+        type: "GET",
+        url: "/product/list",
+        dataType: "json",
+        success: function(resultado){
+            $("#tblproduct > tbody").html("");
+            $.each(resultado, function(index, value){
+                $("#tblproduct > tbody").append(`<tr>`+
+                `<td>${value.productid}</td>`+
+                `<td>${value.productname}</td>`+
+                `<td>${value.unitprice}</td>`+
+                `<td>${value.category.categoryname}</td>`+
+                `<td>${value.supplier.companyname}</td>`+
+                `<td><button type="button" class="btn btn-info btnactualizar" `+
+                ` data-prodcod="${value.productid}" `+
+                ` data-prodname="${value.productname}" `+
+                ` data-produnitprice="${value.unitprice}" `+
+                ` data-proddiscontinued="${value.discontinued}" `+
+                ` data-prodcategory="${value.category.categoryid}" `+
+                ` data-prodsupplier="${value.supplier.supplierid}">Actualizar`+
+                `</button></td></tr>`);
+            })
+        }
+    });
 }
