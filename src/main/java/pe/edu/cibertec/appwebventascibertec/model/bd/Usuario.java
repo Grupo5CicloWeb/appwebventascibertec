760cb97723c32905b9af068bd2108db7f1c029ce
Grupo5CicloWeb <cicloc504@gmail.com>
2024-05-04 11:37:44 -0500

    modelo seguridad y repositorio

---
+++ src/main/java/pe/edu/cibertec/appwebventascibertec/model/bd/Usuario.java
@@ -0,0 +1,27 @@
+package pe.edu.cibertec.appwebventascibertec.model.bd;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idusuario;
+    private String nomusuario;
+    private String email;
+    private String password;
+    private String nombres;
+    private String apellidos;
+    private Boolean activo;
+    @ManyToMany(cascade = CascadeType.MERGE,
+    fetch = FetchType.EAGER)
+    @JoinTable(name = "usuario_rol",
+    joinColumns = @JoinColumn(name = "idusuario"),
+    inverseJoinColumns = @JoinColumn(name = "idrol"))
+    private Set<Rol> roles;
+}
---
+++ src/main/java/pe/edu/cibertec/appwebventascibertec/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebventascibertec.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.cibertec.appwebventascibertec.model.bd.Usuario;
+
+@Repository
+public interface UsuarioRepository
+        extends JpaRepository<Usuario, Integer> {
+    Usuario findByEmail(String email);
+    Usuario findByNomusuario(String nomusuario);
+
+}
